1dc36bb314ef2d010c4e40182d80d30e41f5390e
Ubuntu <ubuntu@ip-10-244-134-173.ec2.internal>
2013-03-24 18:55:32 +0000

    new tactic: openvpn added

---
+++ openvpn_19.3/openvpn_server_me/server.conf.sh
@@ -0,0 +1,20 @@
+#!/bin/sh 
+
+#removing already existing server.conf file and creating a new one
+if [ -e $HOME/ov_me/server.conf ]; then
+	rm -rf $HOME/ov_me/server.conf
+fi
+
+#give port number on which server should listen, 5060 here
+port=$1
+
+cat `dirname $0`/server.conf.template |\
+    sed -e "s/\\\$port\\\$/$port/g" |\
+    tee $HOME/ov_me/server.conf
+
+#/etc/init.d/openvpn start
+
+cd $HOME/ov_me
+
+#starting openvpn for server
+openvpn server.conf
