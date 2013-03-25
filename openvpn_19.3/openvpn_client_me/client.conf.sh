7728df7952b40f9773682c74025f9838f34376b5
bishneet <bishneet@bishneet.(none)>
2013-03-25 00:46:45 +0000

    openvpn added

---
+++ openvpn_19.3/openvpn_client_me/client.conf.sh
@@ -0,0 +1,22 @@
+#!/bin/sh 
+
+if [ -e $HOME/ov_me_client/client.conf ]; then
+	rm -rf $HOME/ov_me_client/client.conf
+fi
+
+ip=$1
+port=$2
+
+cat `dirname $0`/client.conf.template |\
+    sed -e "s/\\\$port\\\$/$port/g" -e "s/\\\$ip\\\$/$ip/g" -e "s/\\\$su\\\$/$SUDO_USER/g"|\
+    tee $HOME/ov_me_client/client.conf
+
+#/etc/init.d/openvpn start
+
+echo "openvpn started"
+
+cd $HOME/ov_me_client
+
+echo "cding"
+
+openvpn client.conf
