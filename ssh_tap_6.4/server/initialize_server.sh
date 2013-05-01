0be7159fac5f14011aba576c1199dca5665f55e8
bishneet <bishneet@bishneet.(none)>
2013-05-01 17:50:14 +0100

    modified ssh server

--- ssh_tap_6.4/server/initialize_server.sh
+++ ssh_tap_6.4/server/initialize_server.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 
-dev_num=$(cat `dirname $0`/parameters | grep Dev_num | awk '{ print $2 }')
+dev_num1=$(cat `dirname $0`/parameters | grep Dev_num1 | awk '{ print $2 }')
+dev_num2=$(cat `dirname $0`/parameters | grep Dev_num2 | awk '{ print $2 }')
 
-tunctl -t tap$dev_num -u $SUDO_USER
+tunctl -t tap$dev_num1 -u $SUDO_USER
+tunctl -t tap$dev_num2 -u $SUDO_USER
 
-ifconfig tap$dev_num 10.0.0.100/24 up
+ifconfig tap$dev_num1 10.0.0.100/24 up
+ifconfig tap$dev_num2 192.16.1.1/24 up
