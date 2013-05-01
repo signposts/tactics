7c185078acea207a36836a486665ede89443b04e
bishneet <bishneet@bishneet.(none)>
2013-05-01 14:10:56 +0100

    ssh key added for connecting to server

--- openvpn_19.3/client/aprior.sh
+++ openvpn_19.3/client/aprior.sh
@@ -4,6 +4,8 @@
 
 host_name=$(cat $HOME/tactics/config | grep server_name | awk '{ print $2 }')
 host_ip=$(cat $HOME/tactics/config | grep server_ip | awk '{ print $2 }')
+su=$(cat `dirname $0`/parameters | grep Host_name | awk '{ print $2 }')
+key=$(cat $HOME/tactics/config | grep path_to_key | awk '{ print $2 }')
 
 if [ -d $HOME/ov_me_client ]; then
 	rm -rf $HOME/ov_me_client
@@ -11,14 +13,18 @@ fi
 
 mkdir $HOME/ov_me_client
 
-scp -i ~/.ssh/bishkey.pem -r $host_name@$host_ip:/home/$host_name/ov_me/ca.crt $HOME/ov_me_client/
+#scp -i ~/.ssh/bishkey.pem -r $host_name@$host_ip:/home/$host_name/ov_me/ca.crt $HOME/ov_me_client/
+scp -i $key -r $host_name@$host_ip:/home/$host_name/ov_me/ca.crt $HOME/ov_me_client/
 echo "ca.crt copied"
 
-scp -i ~/.ssh/bishkey.pem -r $host_name@$host_ip:/home/$host_name/ov_me/keys/$SUDO_USER.crt $HOME/ov_me_client/
-echo "$SUDO_USER.crt copied"
+#scp -i ~/.ssh/bishkey.pem -r $host_name@$host_ip:/home/$host_name/ov_me/keys/$su.crt $HOME/ov_me_client/
+scp -i $key -r $host_name@$host_ip:/home/$host_name/ov_me/keys/$su.crt $HOME/ov_me_client/
+echo "$su.crt copied"
 
-scp -i ~/.ssh/bishkey.pem -r $host_name@$host_ip:/home/$host_name/ov_me/keys/$SUDO_USER.key $HOME/ov_me_client/
-echo "$SUDO_USER.key copied"
+#scp -i ~/.ssh/bishkey.pem -r $host_name@$host_ip:/home/$host_name/ov_me/keys/$su.key $HOME/ov_me_client/
+scp -i $key -r $host_name@$host_ip:/home/$host_name/ov_me/keys/$su.key $HOME/ov_me_client/
+echo "$su.key copied"
 
-scp -i ~/.ssh/bishkey.pem -r $host_name@$host_ip:/home/$host_name/ov_me/ta.key $HOME/ov_me_client/
+#scp -i ~/.ssh/bishkey.pem -r $host_name@$host_ip:/home/$host_name/ov_me/ta.key $HOME/ov_me_client/
+scp -i $key -r $host_name@$host_ip:/home/$host_name/ov_me/ta.key $HOME/ov_me_client/
 echo "ta.key copied"
