362b349912a1e31c6fbdaac124ec07f06dc0e581
bishneet <bishneet@bishneet.(none)>
2013-06-03 13:08:01 +0100

    comments added

--- client.sh
+++ client.sh
@@ -1,21 +1,22 @@
 #!/bin/sh
 
+# This shell script will start all the available tactics at client side simultaneously, creating all the tunnels by the available tactics. 
+
+#This will look for all the tactics folder present in 'working' directory
 for dir in $HOME/tactics/working/*/
 do
-    #dir=${dir%*/}
-    #echo ${dir##*/}
-	echo $dir	
-	#sh $dir/install.sh
-	#echo "starting apriror"
-	if [ -e $dir/client/aprior.sh ]; then
+    	echo $dir	
+	#sh $dir/install.sh     # UNCOMMENT this if you want to install softwares for tactics
+	
+	if [ -e $dir/client/aprior.sh ]; then  #Search for any aprior.sh script which need to run before main script 
 		sh $dir/client/aprior.sh
 	fi
-	#echo "starting initialize"
-	if [ -e $dir/client/initialize_client.sh ]; then
+	
+	if [ -e $dir/client/initialize_client.sh ]; then #Running main script which will start client
 		sh $dir/client/initialize_client.sh
 	fi
-	#echo "ending initialize"
-	sleep 10
+	
+	sleep 10  #Sleep added so that tactic which was started before the other tactic is completed
 done
 
 
--- server.sh
+++ server.sh
@@ -1,21 +1,22 @@
 #!/bin/sh
 
+# This shell script will start all the available tactics at server side simultaneously, creating all the tunnels by the available tactics. 
+
+#This will look for all the tactics folder present in 'working' directory
 for dir in $HOME/tactics/working/*/
 do
-    #dir=${dir%*/}
-    #echo ${dir##*/}
-	echo $dir	
-	#sh $dir/install.sh
-	#echo "starting apriror"
-	if [ -e $dir/server/aprior.sh ]; then
+    	echo $dir	
+	#sh $dir/install.sh	# UNCOMMENT this if you want to install softwares for tactics
+	
+	if [ -e $dir/server/aprior.sh ]; then	#Search for any aprior.sh script which need to run before main script
 		sh $dir/server/aprior.sh
 	fi
-	#echo "starting initialize"
-	if [ -e $dir/server/initialize_server.sh ]; then
+	
+	if [ -e $dir/server/initialize_server.sh ]; then	#Running main script which will start server
 		sh $dir/server/initialize_server.sh
 	fi
-	#echo "ending initialize"
-	sleep 10
+	
+	sleep 10	#Sleep added so that tactic which was started before the other tactic is completed
 done
 
 
